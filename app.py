3adcfa0c30f6b6156c8ef89afd1bf3051351604e
iLaRae Codes <115182525+iLaRae@users.noreply.github.com>
2024-07-16 16:57:47 -0700

    Initial commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 import openai
 from flask_cors import CORS
+from openai import OpenAI
 import os
 
 # Load OpenAI API key from environment variable
@@ -9,26 +10,28 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 # app instance
 app = Flask(__name__)
 CORS(app)
+client = OpenAI()
 
 #------------------------QUESTION---------------
-
 @app.route('/question', methods=['POST'])
 def question():
     data = request.json
-    question = data.get('question')
+    question = data['question']
     
     if question:
-        completion = openai.ChatCompletion.create(
+        completion = client.chat.completions.create(
             model="gpt-4",
             messages=[
+
                 {
                     "role": "user", 
                     "content": question
                 }
+
             ]
         )
 
-        answer = completion.choices[0].message['content']
+        answer = completion.choices[0].message.content
         return jsonify({"answer": answer})
 
     return jsonify({"error": "No question provided"})
